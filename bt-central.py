8331dd9a288c1d5bec10803d630779849a887681
David Liu <davidliu081001@gmail.com>
2022-08-16 16:41:54 -0400

    add central / peripheral code

---
+++ bt-central.py
@@ -0,0 +1,71 @@
+from bleak import BleakClient, BleakScanner, BleakError
+
+import struct
+import asyncio
+
+_ADDRESSES = {"A8:03:2A:6A:36:E6", "B8:27:EB:F1:28:DD"}
+_HANDLES = set() 
+
+_COMM_UUID = "26c00001-ece0-4f7a-b663-223de05387cc"
+_COMM_RW_UUID = "26c00002-ece0-4f7a-b663-223de05387cc"
+
+def address_filter(x):
+    return x.address in _ADDRESSES
+
+# helper to print advertisement data
+def print_ad_data(data):
+    if data.local_name:
+        print(f"\tName: {data.local_name}")
+    if data.service_uuids:
+        print("\tServices:")
+        for service in data.service_uuids:
+            print(f"\t- {service}")
+
+# decode value from characteristic
+def decode(data):
+    return struct.unpack("<h", data)[0] / 100
+
+def scan_handler(device, data):
+    print(f"Found '{device.address}'")
+    print_ad_data(data)
+
+def notification_handler(sender, data):
+    print(f"{sender}: {decode(data)}")
+
+def disconnect_handler(client):
+    print(f"Disconnected from {client.address}")
+
+async def scan(timeout=5.0):
+    scanner = BleakScanner(detection_callback=scan_handler)
+
+    print("Starting scan...")
+    await scanner.start()
+    await asyncio.sleep(timeout)
+    await scanner.stop()
+    print("Scan finished.")
+
+    return filter(address_filter, scanner.discovered_devices)
+
+async def connect_to_device(device):
+    print(f"Connecting to {device}...")
+    async with BleakClient(
+        device, timeout=5.0, disconnected_callback=disconnect_handler
+    ) as client:
+        print(f"Connected to {client.address}")
+        try:
+            await client.start_notify(_COMM_RW_UUID, notification_handler)
+            while True:
+                try:
+                    await asyncio.sleep(0.5)
+                except KeyboardInterrupt:
+                    print("shutting down central...")
+                    return
+        except BleakError:
+            print(f"{client.address} does not contain necessary characteristic")
+
+async def main():
+    devices = await scan()
+    await asyncio.gather(*(connect_to_device(dev) for dev in devices))
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ bt-peripheral.py
@@ -0,0 +1,57 @@
+import aioble
+import uasyncio as asyncio
+import bluetooth as bt
+
+import random
+import struct
+
+# UUID constants
+_COMM_UUID = bt.UUID("26c00001-ece0-4f7a-b663-223de05387cc")
+_COMM_RW_UUID = bt.UUID("26c00002-ece0-4f7a-b663-223de05387cc")
+
+# register GATT service
+comm_service = aioble.Service(_COMM_UUID)
+comm_characteristic = aioble.Characteristic(
+        comm_service,
+        _COMM_RW_UUID,
+        read=True,
+        write=True,
+        notify=True
+)
+aioble.register_services(comm_service)
+
+# encode data to characteristic
+def encode(data):
+    return struct.pack("<h", int(data * 100))
+
+# write to characteristic
+async def write(conn):
+    # slight delay to avoid multi-connection issues
+    #await asyncio.sleep(3.0)
+
+    t = 50
+    while True:
+        # FOR TESTING PURPOSES, REMOVE LATER
+        t += random.uniform(-0.5, 0.5)
+        print(f"Writing '{t}' to peripheral...")
+        comm_characteristic.write( encode(t) )
+        comm_characteristic.notify(conn)
+        await asyncio.sleep_ms(500)
+
+# advertise services
+async def advertise():
+    while True:
+        async with await aioble.advertise(
+                250000, # advertising interval (ms)
+                name="ESP32",
+                services=[_COMM_UUID],
+        ) as connection:
+            print(f"Connection from {connection.device}")
+            await write(connection)
+            await connection.disconnected(timeout_ms=None)
+
+async def main():
+    asyncio.run(advertise())
+
+if __name__ == "__main__":
+    asyncio.run(main())
