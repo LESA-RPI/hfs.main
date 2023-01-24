4ee66cd727d713538e76b0d9f40f14ea1066dc27
Dru Ellering <54593927+dru26@users.noreply.github.com>
2023-01-24 12:21:54 -0500

    Added test files to the client

--- sensor/client/main.py
+++ sensor/client/main.py
@@ -1,65 +1,12 @@
 # Bluetooth peripheral code for fluorescence sensor 
 
-import aioble
-import uasyncio as asyncio
-import bluetooth as bt
+import asyncio
+import ble_service
+import tests
 
-from machine import Pin, ADC
+#tests.startDistanceTest()
+#tests.startFarfieldTest()
+#tests.startFrequencyTest()
+#tests.startNoiseTest()
 
-import random
-import struct
-
-
-
-# UUID constants
-_COMM_UUID = bt.UUID("26c00001-ece0-4f7a-b663-223de05387cc")
-_COMM_RW_UUID = bt.UUID("26c00002-ece0-4f7a-b663-223de05387cc")
-
-# register GATT service
-comm_service = aioble.Service(_COMM_UUID)
-comm_characteristic = aioble.Characteristic(
-        comm_service,
-        _COMM_RW_UUID,
-        read=True,
-        write=True,
-        notify=True
-)
-
-aioble.register_services(comm_service)
-
-# set up pins
-measured_result = ADC(Pin(14))
-# tof_scl = Pin(22
-# tof_sda = Pin(21
-# tof_shutdown = Pin(19
-# 
-
-def encode(data):
-    return struct.pack("<h", int(data * 100))
-
-async def write(conn):
-    t = 50
-    while True:
-        t = measured_result.read_u16()
-        print(f"Writing '{t}' to peripheral...")
-        comm_characteristic.write( encode(t) )
-        comm_characteristic.notify(conn)
-        await asyncio.sleep_ms(500) # change time accordingly
-
-async def advertise():
-    while True:
-        async with await aioble.advertise(
-                250000, # advertising interval (ms)
-                name="ESP32",
-                services=[_COMM_UUID],
-        ) as connection:
-            print(f"Connection from {connection.device}")
-            await write(connection)
-            await connection.disconnected(timeout_ms=None) # may need timeout later
-
-async def main():
-    asyncio.run(advertise())
-
-asyncio.run(main())
-
-
+asyncio.run(ble_service.main())
---
+++ sensor/client/tests/__init__.py
@@ -0,0 +1,4 @@
+from .distance import start as startDistanceTest
+from .farfield import start as startFarfieldTest
+from .frequency import start as startFrequencyTest
+from .noise import start as startNoiseTest
