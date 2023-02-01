40c3128575a8e7adc1c390bd64427129108c4fd7
Dru Ellering <54593927+dru26@users.noreply.github.com>
2023-02-01 14:52:32 -0500

    updated install

--- sensor/client/boot.py
+++ sensor/client/boot.py
@@ -1,1 +1,2 @@
 #import peripheral 
+print(2)
--- sensor/server/install.sh
+++ sensor/server/install.sh
@@ -13,6 +13,16 @@ echo "${YELLOW}WARNING: installation can take upwards of an hour on a new build 
 echo "${YELLOW}WARNING: it is reccomended that you use screen to run this command${NC}"
 echo "${BLUE}INFO: this installation is only for developers and rebuilds all required packages from scratch${NC}"
 
+while true
+do
+    read -r -p 'Do you want to continue? (Y/n) ' choice
+    case "$choice" in
+      n|N) return;;
+      y|Y) break;;
+      *) break;;
+    esac
+done
+
 # Increase the number of retries
 echo 'Acquire::Retries "50";' > "/etc/apt/apt.conf.d/80-retries"
 
@@ -26,10 +36,40 @@ sudo apt install wget -y
 sudo apt install ca-certificates -y
 sudo apt install software-properties-common -y # need add-apt-repository
 sudo apt install libatlas-base-dev -y # this is often missing on Raspberry Pi and is required for numpy
+sudo apt install git -y
 sudo apt update
 
-# install python3.9
-sudo apt install python3.9
+# install pyenv
+#if ! type "pyenv" > /dev/null; then
+#	curl https://pyenv.run | bash
+#	export PATH="${HOME}/.pyenv/bin:$PATH"
+#	eval "$(pyenv init -)"
+#	eval "$(pyenv virtualenv-init -)"
+#fi
+
+# https://pycom.io/wp-content/uploads/2020/04/Lesson-2-Setting-up-Pymakr-LoPy4.pdf
+# install the python version we want
+sudo apt install python3 -y
+sudo apt install python3-dev -y
+
+version=$(python3 -V)
+expected_version="Python 3.9.2"
+if [[ $version != *"$expected_version"* ]]; then
+	echo "${YELLOW}WARNING: default python3 is not 3.9.2, we need to rebuild it from the source${NC}"
+    curl https://pyenv.run | bash
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+	echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+	exec $SHELL
+	pyenv install 3.9.2
+	pyenv global 3.9.2
+fi
+
+#sudo apt install virtualenv
+#virtualenv 
+#sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
+#pyenv install 3.9.2
+#pyenv global 3.9.2
 #if ! type "$python3.9" > /dev/null; then
 	
 	#wget "https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tar.xz"
@@ -45,20 +85,23 @@ sudo apt install python3.9
 #fi
 
 # install pip3.9
-if ! type "pip3.9" > /dev/null; then
-	wget -P "/usr/local/src/hfs" "https://bootstrap.pypa.io/get-pip.py"
-	sudo python3.9 "/usr/local/src/hfs/get-pip.py"
-	pip3.9 install --upgrade setuptools
-	rm -rvf "/usr/local/src/hfs/get-pip.py"
-	#export PATH=/usr/lib/postgresql/14/bin/:$PATH
-	# pip install -r packageName/requrirements.txt
-fi
+#if ! type "pip3.9" > /dev/null; then
+#	wget -P "/usr/local/src/hfs" "https://bootstrap.pypa.io/get-pip.py"
+#	sudo python3.9 "/usr/local/src/hfs/get-pip.py"
+#	pip install --upgrade setuptools
+#	rm -rvf "/usr/local/src/hfs/get-pip.py"
+#	#export PATH=/usr/lib/postgresql/14/bin/:$PATH
+#	# pip install -r packageName/requrirements.txt
+#fi
 
 # Move the index file to public
 mv -v "/usr/local/src/hfs/index.html" "/usr/local/src/hfs/public/index.html" 
 
 # install node
+
 if ! type "node" > /dev/null; then
+	echo "${BLUE}INFO: installing Node.js${NC}"
+
 	echo "Installing Node.js v16 from the unoffical arm6l release following this https://gist.github.com/davps/6c6e0ba59d023a9e3963cea4ad0fb516 guide"
 	wget -P "/usr/local" https://unofficial-builds.nodejs.org/download/release/v16.9.1/node-v16.9.1-linux-armv6l.tar.gz
 	tar -xvzf "/usr/local/node-v16.9.1-linux-armv6l.tar.gz" -C "/usr/local"
@@ -70,22 +113,20 @@ if ! type "node" > /dev/null; then
 	rm -rvf /usr/local/node-v16.9.1-linux-armv6l
 fi
 
-echo "${BLUE} INFO Reinstalling project dependencies${NC}"
+echo "${BLUE}INFO: reinstalling project dependencies${NC}"
 
 # install the required node packages
 npm --prefix "/usr/local/src/hfs" install "/usr/local/src/hfs"
-sudo rm -r "./node_modules/undefined"
-sudo rm -d "./node_modules"
 
 # install all requirements for the Bluetooth connectivity
-pip3.9 install bleak==0.14.* --force-reinstall -vvv
+pip install bleak==0.14.* --force-reinstall -vvv
 
 # install all requirements for local webserver
 # NOTE: compatible numpy versions can be found here https://github.com/matplotlib/matplotlib/blob/ac3d0caf0007389579a5fa2576d95657b03d3f02/doc/devel/min_dep_policy.rst#id1
-pip3.9 install matplotlib==3.6.* --force-reinstall -vvv
-pip3.9 uninstall numpy
-apt install python3-numpy
-python3.9 -c "import matplotlib"
+pip install matplotlib==3.6.* --force-reinstall -vvv
+pip uninstall numpy
+apt install python3-numpy -y
+python -c "import matplotlib"
 
 # install psql
 if ! type "psql" > /dev/null; then
@@ -93,7 +134,7 @@ if ! type "psql" > /dev/null; then
 	wget --quiet -O - "https://www.postgresql.org/media/keys/ACCC4CF8.asc" | sudo apt-key add -
 	sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 	sudo apt update
-	sudo apt install postgresql-13
+	sudo apt install postgresql-13 -y
 	sudo sed -i 's/local   all             postgres                                peer/local   all             postgres                                trust/' '/etc/postgresql/14/main/pg_hba.conf'
 	sudo sed -i 's/local   all             all                                     peer/local   all             all                                     trust/' '/etc/postgresql/14/main/pg_hba.conf'
 	sudo passwd -d postgres
@@ -116,8 +157,8 @@ sudo apt autoremove -y
 echo 'Acquire::Retries "3";' > "/etc/apt/apt.conf.d/80-retries"
 
 # verify installation
-python3.9 -V
-pip3.9 -V
+python -V
+pip -V
 node -v
 output=$(node -v)
 if [ "$output" != "v16.9.1" ]; then
