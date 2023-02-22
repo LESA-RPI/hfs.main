2bbf52fc0132b8bc4405507d61d4420b9c321043
GenieInABottle64626 <112441130+GenieInABottle64626@users.noreply.github.com>
2023-02-22 13:18:16 -0500

    disconnect

--- sensor/client/boot.py
+++ sensor/client/boot.py
@@ -41,20 +41,19 @@ print('[INFO] Network config {}'.format(sta_if.ifconfig()))
 # check for updates
 if (CHECK_FOR_UPDATES):
     print("[INFO] Checking for updates...")
-    #try: 
+    try: 
         # monitor memory
-    import time, machine, gc
-    import ota_update.updater as ota #import ota_updater# ota# OTAUpdater
-    time.sleep(1)
-    print('[INFO] Memory free {}'.format(gc.mem_free()))
-    # download and install an update if availible
-    ota_updater = ota.OTAUpdater(GITHUB_URL, github_src_dir=GITHUB_SRC_DIR, dev_version_url=DEV_VERSION_FILE, update_sub_dir=UPDATE_SUB_DIR, main_dir=GITHUB_BRANCH)
-    update = ota_updater.dev_install_update_if_available()
-    del(ota_updater)
-    if (update):
-        print("[INFO] Upgrade complete, reseting...")
-        machine.reset()
-    #except Exception as e:
-    #    print("WARNING: Update failed ({})".format(e))
-import webrepl
-webrepl.start()
+        import time, machine, gc
+        import ota_update.updater as ota #import ota_updater# ota# OTAUpdater
+        time.sleep(1)
+        print('[INFO] Memory free {}'.format(gc.mem_free()))
+        # download and install an update if availible
+        ota_updater = ota.OTAUpdater(GITHUB_URL, github_src_dir=GITHUB_SRC_DIR, dev_version_url=DEV_VERSION_FILE, update_sub_dir=UPDATE_SUB_DIR, main_dir=GITHUB_BRANCH)
+        update = ota_updater.dev_install_update_if_available()
+        del(ota_updater)
+        if (update):
+            print("[INFO] Upgrade complete, reseting...")
+            machine.reset()
+    except Exception as e:
+        print("WARNING: Update failed ({})".format(e))
+sta_if.disconnect()
