1104d9cc84265204533d8aa12344bab3c48fc143
Dru Ellering <54593927+dru26@users.noreply.github.com>
2023-03-15 11:32:19 -0400

    update

--- sensor/server/install.sh
+++ sensor/server/install.sh
@@ -177,7 +177,7 @@ su - postgres -c 'psql -c "TABLE data"'
 sudo systemctl start hfs.service
 
 # add the 'update' command
-u_cmd = 'alias update-hfs="wget -O hfs.zip https://github.com/LESA-RPI/hfs.main/archive/refs/heads/server-ui.zip\ && sudo unzip -o -j hfs.zip hfs.*/sensor/server/* -d /usr/local/src/hfs/ && rm hfs.zip && (cd /usr/local/src/hfs && sudo mkdir -p public && sudo mv -v *.html public/ && sudo mv -v *.css public/ && sudo touch public/public.log && sudo npm install)"'
+u_cmd = 'alias update-hfs="wget -O hfs.zip https://github.com/LESA-RPI/hfs.main/archive/refs/heads/server-ui.zip && sudo unzip -o -j hfs.zip hfs.*/sensor/server/* -d /usr/local/src/hfs/ && rm hfs.zip && (cd /usr/local/src/hfs && sudo mkdir -p public && sudo mv -v *.html public/ && sudo mv -v *.css public/ && sudo touch public/public.log && sudo npm install)"'
 sudo sh -c "echo ${u_cmd} >> /etc/bash.bashrc"
 source /etc/bash.bashrc
 
