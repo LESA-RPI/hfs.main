fd126ac43665df5e14385035066f4a4aef69ee36
GenieInABottle64626 <112441130+GenieInABottle64626@users.noreply.github.com>
2023-03-15 15:13:55 -0400

    change

--- sensor/client/prgm_frequency.py
+++ sensor/client/prgm_frequency.py
@@ -2,6 +2,7 @@ import uasyncio
 import math
 import machine
 from machine import Timer
+from uasyncio import Event
 
 import device_sensor as sensor # sensor.readAndSend(server, pipe)
 import device_pins as pins
@@ -9,7 +10,7 @@ import device_pins as pins
 frequency_list = [15, 50, 100, 200, 400, 800, 1000, 1200, 1400, 600, 700, 650]
 led_on_time = 1000 #1 sec
 led_off_start_time = 10*60*1000 #10 min
-led_off_time = 10000 #5 min
+led_off_time = 100 #5 min
 #led_off_time = 5*60*1000 #5 min
 led_between_measure_time = 800 #TT
 led_wait_measure_time = 200
@@ -17,43 +18,39 @@ samples = 5
 counter = 0
 results = []
 average_result = 0
+LEDON = True   
 
-async def led_blinking(led, freq):
-    while counter < led_off_time:
-        led.on()
-        await uasyncio.sleep_ms(round((1/freq)*10**3))
-        print("LED ", led, " on")
-        led.off()
-        await uasyncio.sleep_ms(round((1/freq)*10**3))
-        print("LED", led, " off")
-
-async def cycle(led1, led2, led3, led4, freq):
-    task1 = uasyncio.create_task(led_blinking(led1, freq))
-    task2 = uasyncio.create_task(led_blinking(led2, freq))
-    task3 = uasyncio.create_task(led_blinking(led3, freq))
-    task4 = uasyncio.create_task(led_blinking(led4, freq))
-    await task1
-    await task2
-    await task3
-    await task4
+async def led_blinking():
+    if LEDON == True:
+        pins.LED1.on()
+        pins.LED2.on()
+        pins.LED3.on()
+        pins.LED4.on()
+        print("on")
+    else:
+        pins.LED1.off()
+        pins.LED2.off()
+        pins.LED3.off()
+        pins.LED4.off()
+        print("off")
 
 
-def sleep():
+def sleep(curFreq):
     global counter, led_off_time, led_wait_measure_time, results, led_between_measure_time
-    while(counter >= led_off_time):
+    while(counter >= led_off_time*curFreq):
         pins.LED1.off()
         pins.LED2.off()
         pins.LED3.off()
         pins.LED4.off()
         counter = 0
     #machine.lightsleep(led_wait_measure_time)
-    sleep_between_measurements(led_wait_measure_time)
+    sleep_between_measurements(led_wait_measure_time*curFreq)
     for i in range(0, samples):
         results.append(sensor.readPhotodiode())
         print("Results ", results[i])
         #total_result = total_result + results[i]
         #machine.lightsleep(led_between_measure_time)
-        sleep_between_measurements(led_between_measure_time)
+        sleep_between_measurements(led_between_measure_time*curFreq)
     #average_result = total_result/samples
     #print("Average result: ",average_result)
     #total_result = 0
@@ -61,9 +58,13 @@ def sleep():
     sleep_between_measurements(led_off_time)
     counter = 0 #maybe before deep sleep?
 
-def timerCallback(self):
+def timerCallback(self, curFreq, event):
     global counter
+    led_blinking()
+    LEDON = not LEDON
     counter = counter + 1 #increments counter used for determining when to stop measurements
+    if counter == curFreq*led_on_time:
+        event.set()
     print(counter)
 
 async def sleep_between_measurements(led_time):
@@ -75,17 +76,19 @@ async def run(server, pipe, data: int):
     print("[prgm_frequency] start")
     await sleep_between_measurements(led_off_time) #sleeps for 5 minutes
     for curFreq in frequency_list: #all the frequencies in test
-        curFreq = .1
         try:
+            event = uasyncio.Event()
             tim = Timer(1) #timer used to count when LED sleeps and takes measurements
-            tim.init(freq = curFreq, mode = Timer.PERIODIC, callback = timerCallback)
+            tim.init(freq = curFreq, mode = Timer.PERIODIC, callback = timerCallback(curFreq, event))
+            await event
+            tim.deinit()
+            event.clear()
             #tim.channel(channel = machine.TIMER.A, freq = curFreq, mode = Timer.PWM, pulse_width_percent=50)
         except Exception as error:
             print(error)
             pass
         print(counter)
-        await cycle(pins.LED1, pins.LED2, pins.LED3, pins.LED4, curFreq) #threads LEDs blinking
-        sleep() #sleeps for 10 minutes
+        sleep(curFreq) #sleeps for 10 minutes
     #pipe.notify(server)
     print("[prgm_frequency] stop")
     
