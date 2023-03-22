266a8ef45b824858681aeb450cc7c118037ddf85
GenieInABottle64626 <112441130+GenieInABottle64626@users.noreply.github.com>
2023-03-22 16:29:08 -0400

    change

--- sensor/client/device_sensor.py
+++ sensor/client/device_sensor.py
@@ -16,6 +16,7 @@ def readSonar():
     return 0
 
 def readAndSend(server, pipe):
+    print(f"Start taking measurement")
     distance, chlf = readSonar(), readPhotodiode()
     print(f"Writing '{distance}', {chlf} to peripheral...")
     try:
