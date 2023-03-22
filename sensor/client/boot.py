c71ef73096cf2a09656b9e83e1b4b8b9ba2010e2
GenieInABottle64626 <112441130+GenieInABottle64626@users.noreply.github.com>
2023-03-22 14:52:38 -0400

    change

--- sensor/client/boot.py
+++ sensor/client/boot.py
@@ -1,6 +1,6 @@
 print("[INFO] Booting device...")
 
-CHECK_FOR_UPDATES = False
+CHECK_FOR_UPDATES = True
 LAN_SSID = "SLERC3"
 LAN_PASSWORD = "ganlightemittingdiode"
 GITHUB_URL = "https://github.com/LESA-RPI/hfs.main"
