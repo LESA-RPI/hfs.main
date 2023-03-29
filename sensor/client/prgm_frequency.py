9f08d54a9399e77aab6daaa2f9482eeac8af4343
GenieInABottle64626 <112441130+GenieInABottle64626@users.noreply.github.com>
2023-03-29 13:06:23 -0400

    change

--- sensor/client/prgm_frequency.py
+++ sensor/client/prgm_frequency.py
@@ -8,9 +8,8 @@ import device_pins as pins
 
 frequency_list = [15, 50, 100, 200, 400, 800, 1000, 1200, 1400, 600, 700, 650]
 led_on_time = 1.5 #10 sec
-led_off_start_time = 10*60*1000 #10 min
-#led_off_time = 10 #10 sec
-led_off_time = 5*60*1000 #5 min
+#led_off_start_time = 10*60*1000 #10 min
+led_off_start_time = 60 #1 min for testing purposes
 samples = 5
 led_wait_measure_time = 0.6
 led_measurement_time = led_on_time-led_wait_measure_time
@@ -37,25 +36,18 @@ async def measurements(server, pipe):
     print("starting measurements")
     for i in range(0, samples):
         sensor.readAndSend(server, pipe)
-        #total_result = total_result + results[i]
-        #machine.lightsleep(led_between_measure_time)
         await sleep_between_measurements(led_between_measure_time)
 
 async def sleep(curFreq):
-    global counter, led_off_time, led_wait_measure_time, results, led_between_measure_time
-    while(counter >= led_off_time*curFreq):
+    global counter, led_off_start_time, led_wait_measure_time, results, led_between_measure_time
+    while(counter >= led_off_start_time*curFreq):
         pins.LED1.off()
         pins.LED2.off()
         pins.LED3.off()
         pins.LED4.off()
         print("OFF")
         counter = 0
-    #machine.lightsleep(led_wait_measure_time)
-    #average_result = total_result/samples
-    #print("Average result: ",average_result)
-    #total_result = 0
-    #machine.deepsleep(led_off_time)
-    await sleep_between_measurements(led_off_time)
+    await sleep_between_measurements(led_off_start_time)
     counter = 0 #maybe before deep sleep?
 
 def timerCallback(curFreq, server, pipe):
@@ -79,15 +71,14 @@ async def sleep_between_measurements(led_time):
 # run this program once and only once, server will decide how to loop
 async def run(server, pipe, data: int):    
     print("[prgm_frequency] start")
-    await sleep_between_measurements(led_off_time) #sleeps for 5 minutes
-    loops = 0
+    await sleep_between_measurements(led_off_start_time) #sleeps for 10 minutes
     for curFreq in frequency_list: #all the frequencies in test
         print("Actual frequency: ", curFreq)
         tim = Timer(1) #timer used to count when LED sleeps and takes measurements
         tim.init(freq = curFreq, mode = Timer.PERIODIC, callback = lambda t: timerCallback(curFreq, server, pipe))
         await uasyncio.sleep(led_on_time)
         tim.deinit()
-        await sleep(led_off_time) #sleeps for 10 minutes
-    #pipe.notify(server)
+        await sleep(led_off_start_time) #sleeps for 10 minutes
+    pipe.notify(server)
     print("[prgm_frequency] stop")
     
