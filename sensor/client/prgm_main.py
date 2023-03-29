eb79ca19e374b9755707b91c5ea5ef1ac7ef8ebd
GenieInABottle64626 <112441130+GenieInABottle64626@users.noreply.github.com>
2023-03-29 15:18:13 -0400

    change

--- sensor/client/prgm_main.py
+++ sensor/client/prgm_main.py
@@ -19,28 +19,27 @@ def led_blinking():
         pins.LED2.on()
         pins.LED3.on()
         pins.LED4.on()
-        print("on")
+
     else:
         pins.LED1.off()
         pins.LED2.off()
         pins.LED3.off()
         pins.LED4.off()
-        print("off")
 
-def timerCallback(curFreq, server, pipe):
+def timerCallback(server, pipe):
     global counter, LEDON
     LEDON = not LEDON
     led_blinking()
     counter = counter + 1 #increments counter used for determining when to stop measurements
-    print("counter")
 
-async def measurements1(curFreq, server, pipe):
-    while (counter-1) < round(curFreq*led_wait_measure_time):
+async def measurements1(tim, server, pipe):
+    while (counter-1) < round(frequency*led_wait_measure_time):
         pass
     print("starting measurements")
     for i in range(0, samples):
         sensor.readAndSend(server, pipe)
-        await sleep_between_measurements(led_between_measure_time)  
+        await sleep_between_measurements(led_between_measure_time)
+    tim.deinit() 
 
 async def sleep_between_measurements(led_time):
     await uasyncio.sleep_ms(led_time)
@@ -51,9 +50,10 @@ async def sleep_between_measurements(led_time):
 async def run(server, pipe, data: int):    
     print("[prgm_main] start")
     tim = Timer(1) #timer used to count when LED sleeps and takes measurements
-    tim.init(freq = frequency, mode = Timer.PERIODIC, callback = lambda t: timerCallback(frequency, server, pipe))
-    await measurements1(frequency, server, pipe)
+    tim.init(freq = frequency, mode = Timer.PERIODIC, callback = lambda t: timerCallback(server, pipe))
+    await measurements1(tim, server, pipe)
     # pipe.write("data")
     print("hello world")
+
     pipe.notify(server)
     print("[prgm_main] stop")
